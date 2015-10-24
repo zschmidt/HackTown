f54a55335e12c69984e7df19edb1743ee682d10b
Zach Schmidt <zschmidt@uoregon.edu>
2015-10-24 16:19:57 -0700

    Added boundingBox code

--- bingElevation.js
+++ bingElevation.js
@@ -1,19 +1,32 @@
 var main = function(){
 
-  var lat = document.getElementById("lat").value;
-  var long = document.getElementById("long").value;
+  var east = document.getElementById("east").value;
+  var south = document.getElementById("south").value;
+  var west = document.getElementById("west").value;
+  var north = document.getElementById("north").value;
 
 
-  var apiLocation = "http://dev.virtualearth.net/REST/v1/Elevation/List?points=";
+  var apiLocation = "http://dev.virtualearth.net/REST/v1/Elevation/Bounds?bounds=";
+  var queries = "&rows=32&cols=32&heights=sealevel";
 
   var key = "&key=AgdJmHJNbOApcjOXhgGoeD0OeiaEoxJ-zXbtF60rdVvWnD2GZeH-czRQ9lH03Vil";
+
+
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(0,0,150,75);
+
+
+  var boudingBox = "http://dev.virtualearth.net/REST/v1/Elevation/Bounds?bounds=43.64,-123.671,44.34,-122.557&rows=100&cols=100&heights=sealevel"
+
 
 
   $.ajax(
   {
       type: 'GET',
       cache : false,
-      url : apiLocation+ lat +","+ long  +key,
+      url : apiLocation+ south +","+ west + ","+ north + "," + east + queries +key,
       async: false,
       crossDomain : true,
       success: function (data)
@@ -27,7 +40,7 @@ var main = function(){
               var elevations = resource[j].elevations;
               for(var k = 0; k<elevations.length; k++)
               {
-                document.getElementById("out").innerHTML += "Elevation at "+lat+","+long+" is: "+elevations[k];
+                document.getElementById("out").innerHTML += "Elevation is: "+elevations[k]+"<br>";
               }
             }
 
